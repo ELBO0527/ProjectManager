82f50487fb1c7ac9d71b577d60f3470f57c2a67e
ELBO0527 <63278770+ELBO0527@users.noreply.github.com>
2022-01-19 01:16:56 +0900

    20220119 starting setup

--- src/main/java/LBWprojecter/projectmanager/ProjectmanagerApplication.java
+++ src/main/java/LBWprojecter/projectmanager/ProjectmanagerApplication.java
@@ -2,8 +2,9 @@ package LBWprojecter.projectmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class ProjectmanagerApplication {
 
 	public static void main(String[] args) {
